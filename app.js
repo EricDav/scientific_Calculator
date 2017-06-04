b11c47d132d3c5552e14dc498918b424bbe39cb3
David <alienyidavid4christ@gmail.com>
2017-06-04 02:00:09 -0700

    add subtraction, addition, multiplication and division operation

---
+++ app.js
@@ -0,0 +1,125 @@
+const isDigit = function(ch) {
+ 	return /\d/.test(ch);
+ }
+
+ const divMult = (str) => {
+ 	let result = 1;
+ 	let arr = [];
+ 	let arr1 = [];
+ 	let ans = '';
+ 	for(let i =0; i < str.length; i++ ){
+ 		if(str[i] === 'x' || str[i] === '/'){
+ 			arr.push(ans);
+ 			arr.push(str[i]);
+ 			ans = '';
+ 		}
+ 		else{
+ 			ans +=str[i];
+ 		}
+ 	}
+ 	
+ 	arr.push(ans);
+ 	
+ 	for(let v = 0; v < arr.length; v++){
+
+ 		if(arr[v] === '/'){
+ 			arr1.push(parseFloat(arr[v-1]) /parseFloat(arr[v+1]));
+ 		}
+ 		else if((isDigit(arr[v]) && arr[v+1] !== '/'  && arr[v-1] !== '/') || arr[v] === 'x'){
+ 			arr1.push(arr[v]);
+ 		}
+ 	}
+ 	
+ 	for(let u= 0; u < arr1.length; u++){
+ 		// if(u !== 0 && arr[u-1] === 'x'){
+ 		// 	continue;
+ 		// }
+ 		if(isDigit(arr1[u])){
+ 			result *= parseFloat(arr1[u]);
+ 		}
+ 	}
+ 	return result;
+ };
+const split = (str) => {
+	let has = false;
+	let arr = 0;
+	let ans = '';
+	for(let i=0; i < str.length; i++){
+		if(i === 0 && (str[i] === '+' || str[i] === '-')){
+			ans +=str[i];
+		}
+		else if(isDigit(str[i]) || str[i] === '.' || str[i] === 'x' || str[i] === '/'){
+			ans +=str[i];
+		}
+		else if(str[i] === '+' || str[i] === '-'){
+			if(has){
+				arr+=divMult(ans);
+			}
+			else{
+			  arr+=parseFloat(ans);
+			}
+			has = false;
+			ans = str[i];
+		}
+		if(str[i] === 'x' || str[i] === '/'){
+			has = true;
+		}
+	}
+		if(has){
+				arr+=divMult(ans);
+			}
+			else{
+			  arr+=parseFloat(ans);
+			}
+	if(arr === parseInt(arr)){
+		return parseInt(arr);
+	}
+	return arr;
+};
+
+
+
+class Numbers{
+	constructor(){
+		this.question = '';
+		this.answer = '';
+	}
+	Value(value){
+		if(value === '+' || value === '-' || value === 'x' || value === '/'){
+			this.question = this.question  + ' ' + value + ' ';
+			return this.question;
+		}
+		else{
+			this.question = this.question + value;
+			return this.question;
+		}
+		
+	}
+	getAnswer(){
+		let x = this.question.replace(/\s/g,'');
+		return  split(x);
+	}
+	clear(){
+		this.question = '';
+	}
+}
+
+const Num = new Numbers();
+
+function Number(detail){
+	
+	let number = detail.id;
+	if(number === '=') {
+		let answer = Num.getAnswer();
+		document.getElementById('ans').textContent = answer;
+	}
+	else if(number === 'AC'){
+		Num.clear();
+		document.getElementById('ques').textContent = '';
+		document.getElementById('ans').textContent = '';	
+	}
+	else{
+		let ques = Num.Value(number);
+		document.getElementById('ques').textContent = ques;
+	}
+}
---
+++ helpers/split.js
@@ -0,0 +1,5 @@
+const isDigit = function(ch) {
+ 	return /\d/.test(ch);
+ }
+
+ for
